8fc7f4d1d689b27c2da14e9dff1c6add81200497
khuntkaran <khuntkaran995@gmail.com>
2024-08-13 18:18:22 +0530

    .

---
+++ GNWebForm3C_CodeB/AdminPanel/Report/ACC_IncomeReport.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Default/MasterPage.master" AutoEventWireup="true" CodeFile="ACC_IncomeReport.aspx.cs" Inherits="AdminPanel_Account_ACC_Income_ACC_IncomeReport" %>
+
+<%@ Register Assembly="Microsoft.ReportViewer.WebForms, Version=15.0.0.0, Culture=neutral, PublicKeyToken=89845dcd8080cc91" Namespace="Microsoft.Reporting.WebForms" TagPrefix="rsweb" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="cphPageHeader" runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="cphBreadcrumb" runat="Server">
+</asp:Content>
+<asp:Content ID="Content4" ContentPlaceHolderID="cphPageContent" runat="Server">
+    Report file
+    <asp:ScriptManager ID="sm" runat="server">
+</asp:ScriptManager>
+    <rsweb:ReportViewer ID="rvIncomeList" runat="server" Width="100%" Height="600px" ProcessingMode="Local">
+        <LocalReport ReportPath="AdminPanel/Report/RPT_ACC_Income.rdlc">
+        </LocalReport>
+    </rsweb:ReportViewer>
+</asp:Content>
+<asp:Content ID="Content5" ContentPlaceHolderID="cphScripts" runat="Server">
+</asp:Content>
+
---
+++ GNWebForm3C_CodeB/AdminPanel/Report/ACC_IncomeReport.aspx.cs
@@ -0,0 +1,75 @@
+﻿using GNForm3C.BAL;
+using iTextSharp.text;
+using Microsoft.Reporting.WebForms;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class AdminPanel_Account_ACC_Income_ACC_IncomeReport : System.Web.UI.Page
+{
+    private DataTable dt = new DataTable();
+    private dsIncome dsi = new dsIncome();
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            GenerateReport();
+        }
+    }
+
+    private void GenerateReport()
+    {
+        SqlInt32 IncomeTypeID = SqlInt32.Null;
+        SqlDecimal Amount = SqlDecimal.Null;
+        SqlDateTime IncomeDate = SqlDateTime.Null;
+        SqlInt32 HospitalID = SqlInt32.Null;
+        SqlInt32 FinYearID = SqlInt32.Null;
+        Int32 Offset = 0;
+        Int32 TotalRecords = 0;
+        Int32 TotalPages = 1;
+
+        ACC_IncomeBAL aCC_IncomeBAL = new ACC_IncomeBAL();
+        dt = aCC_IncomeBAL.SelectPage(Offset, 20,out TotalRecords, IncomeTypeID, Amount, IncomeDate, HospitalID, FinYearID);
+        FillDataSet();
+
+    }
+
+    protected void FillDataSet()
+    {
+        int serialNumber = 1; 
+        foreach (DataRow dr in dt.Rows){
+            dsIncome.IncomeRow drACC_Income = dsi.Income.NewIncomeRow();
+
+            drACC_Income.Sr = serialNumber.ToString();
+            serialNumber++;
+            if (!dr["FinYearName"].Equals(System.DBNull.Value))
+                drACC_Income.Fin_Year = Convert.ToString(dr["FinYearName"]);
+            if (!dr["Hospital"].Equals(System.DBNull.Value))
+                drACC_Income.Hospital = Convert.ToString(dr["Hospital"]);
+            if (!dr["IncomeType"].Equals(System.DBNull.Value))
+                drACC_Income.Income_Type = Convert.ToString(dr["IncomeType"]);
+            if (!dr["Amount"].Equals(System.DBNull.Value))
+                drACC_Income.Amount = Convert.ToDecimal(dr["Amount"]);
+            if (!dr["IncomeDate"].Equals(System.DBNull.Value))
+                drACC_Income.Income_Date = Convert.ToDateTime(dr["IncomeDate"]).ToString("dd-mm-yyyy");
+            dsi.Income.Rows.Add(drACC_Income);
+        }
+        SetReportParameter();
+        this.rvIncomeList.LocalReport.DataSources.Clear();
+        this.rvIncomeList.LocalReport.DataSources.Add( new ReportDataSource("dsIncome",(DataTable)dsi.Income) );
+        this.rvIncomeList.LocalReport.Refresh();
+    }
+    private void SetReportParameter()
+    {
+        String ReportTitle = "Income";
+        ReportParameter rptReportTitle = new ReportParameter("ReportTitle",ReportTitle);
+        this.rvIncomeList.LocalReport.SetParameters(new ReportParameter[] {rptReportTitle, });
+    }
+}
